b88ccb5464ab957ffe48b2f191bc33375ce52c91
gabrielxzc <codrincojocaru@yahoo.com>
2018-12-03 17:51:39 +0200

    add get strategies method for backend

--- application/application.py
+++ application/application.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS
 
 from controllers.moves import get_moves_controller
+from controllers.strategies import get_strategies_controller
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +12,8 @@ CORS(app)
 @app.route('/moves', methods=['GET'])
 def get_moves_router():
     return get_moves_controller(request)
+
+
+@app.route('/strategies', methods=['GET'])
+def get_strategies_router():
+    return get_strategies_controller(request)
---
+++ application/controllers/strategies.py
@@ -0,0 +1,12 @@
+from common_lib.package_operations import package_contents
+
+import json
+
+CHESS_ENGINES_PACKAGE = 'chess_engines'
+
+
+def get_strategies_controller(request):
+    answer = [strategy for strategy in package_contents(
+        CHESS_ENGINES_PACKAGE) if strategy != '__init__']
+
+    return json.dumps(answer)
