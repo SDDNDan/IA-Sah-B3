2837d51a829c105331b9c0e34198f679f77018d4
gabrielxzc <codrincojocaru@yahoo.com>
2018-12-03 17:33:45 +0200

    add cors support

--- application/application.py
+++ application/application.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 
 from controllers.moves import get_moves_controller
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/moves', methods=['GET'])
