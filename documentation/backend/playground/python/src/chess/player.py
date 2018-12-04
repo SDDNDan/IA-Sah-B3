636d4f2aee5386f7d27d1998866fb5307dbeddab
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2018-12-04 17:33:44 +0200

    Added own chess library. Included chess engines in cpp. Included universal chess controller Chess.

--- documentation/backend/playground/python/src/chess/player.py
+++
@@ -1,3 +0,0 @@
-class Player:
-    def get_best_move(self, fen):
-        raise NotImplementedError("Player is an interface.")
--- documentation/backend/playground/python/src/testing/chessTest.py
+++
@@ -1,6 +0,0 @@
-from PYTHON.src.chess.chess_game import Chess
-
-chess = Chess()
-chess.move("e2e4")
-print(chess.get_best_move_depth(12))
-print(chess.get_evaluation_depth(15))
