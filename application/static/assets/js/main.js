fbad6610b94bec8e6336f63ed92ec6ac6211bbff
srzvan <sbingu.razvan@gmail.com>
2018-12-05 00:23:29 +0200

    implemented renderStrategiesDetails function + added some relevant bootstrap markup

--- application/static/assets/js/main.js
+++ application/static/assets/js/main.js
@@ -14,15 +14,73 @@ $( document ).ready( function () {
   });
 
   chessboard.start();
-
+  
   // getStrategies() - call
-
+  
   // renderStrategiesDetails( strategiesDetails ) - call
 });
 
 // getStrategies()
 
 // renderStrategiesDetails( strategiesDetails )
+function renderStrategiesDetails ( strategiesDetails ) {
+  // grab the strategies details container
+  const strategiesDetails = document.getElementById('js-strategies-details');
+  // navigate down to the .row descendant
+  const container = strategiesDetails.children[1];
+  const row = container.children[0];
+
+  // .col element
+  let col = document.createElement('div');
+  col.classList = 'col-12 col-md-6';
+
+  // .card
+  let card = createCardMarkup( strategyDetails );
+
+  // append .card to .col
+  col.appendChild( card );
+
+  // append markup to the .row
+  row.appendChild( col );
+}
+
+function createCardMarkup( strategyDetails ) {
+  // Create Bootstrap card markup
+  // https://getbootstrap.com/docs/4.1/components/card/#using-grid-markup
+  // .card element
+  let card = document.createElement('div');
+  card.classList = 'card';
+
+  // .card-body container
+  let cardBody = document.createElement('div');
+  cardBody.classList = 'card-body';
+
+  // .card-title 
+  let cardTitle = document.createElement('h2');
+  cardTitle.classList = 'card-title';
+  cardTitle.innerText = strategyDetails.strategy;
+
+  // .card-text
+  let cardText = document.createElement('p');
+  cardText.classList = 'card-text';
+  cardText.innerText = strategyDetails.description;
+
+  // .btn.btn-primary link to documentation
+  let link = document.createElement('a');
+  link.classList = 'btn btn-primary';
+  link.innerText = 'Documentation';
+  link.href = strategyDetails.documentation;
+
+  // append everything to .card-body
+  cardBody.appendChild( cardTitle );
+  cardBody.appendChild( cardText );
+  cardBody.appendChild( link );
+
+  // append .card-body to .card container
+  card.appendChild( cardBody );
+
+  return card;
+}
 
 // getSuggestedMoves( strategies )
 
