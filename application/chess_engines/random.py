e5973750893924db1c7e7b8886d564c1456f8f5a
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2018-12-06 17:47:54 +0200

    Added random strategy implementation.

--- application/chess_engines/random.py
+++ application/chess_engines/random.py
@@ -1,6 +1,10 @@
+from application.chess.random_player import RandomPlayer
+
+
 def get_strategy_name():
     return "Random"
 
 
 def get_strategy_move(fen):
-    return "d2d4"
+    rand_player = RandomPlayer()
+    return rand_player.get_best_move(fen)
