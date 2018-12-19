5b9098ea863ee58bc122df3afa2481a381d0df53
serbanp <klainse@yahoo.com>
2018-12-19 14:09:17 +0200

    finished hermann

--- application/chess_engines/hermann.py
+++ application/chess_engines/hermann.py
@@ -1,6 +1,11 @@
+from application.chess.engine import Engine
+
+
 def get_strategy_name():
     return "Hermann"
 
 
 def get_strategy_move(fen):
-    return "D2D4"
+    engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_32.exe')
+    #engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_64.exe')
+    return engine.get_best_move(fen)
