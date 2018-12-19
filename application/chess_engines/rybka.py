2dc8cf0a092fb8fbcf7c776e01886edd420fa81d
CCBet <betiucciprian@gmail.com>
2018-12-19 19:02:55 +0200

    Added get_strategy_move for Rybka

--- application/chess_engines/rybka.py
+++ application/chess_engines/rybka.py
@@ -1,6 +1,10 @@
+from application.chess.engine import Engine
+
+
 def get_strategy_name():
     return "Rybka"
 
 
 def get_strategy_move(fen):
-    return "D2D4"
+    engine = Engine(engine_path='../chess_engines_cpp/Rybka/Rybkav2.3.2a.mp.x64.exe')
+    return engine.get_best_move(fen)
