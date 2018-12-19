27ea2fbcc0b611a55ea19b9a78952b60b369fb1f
unknown <sebastian_s97@yahoo.ro>
2018-12-19 14:25:16 +0200

    AnMon

--- application/chess_engines/anmon.py
+++ application/chess_engines/anmon.py
@@ -1,6 +1,10 @@
+from application.chess.engine import Engine
+
+
 def get_strategy_name():
     return "AnMon"
 
 
 def get_strategy_move(fen):
-    return "D2D4"
+    engine = Engine(engine_path='../chess_engines_cpp/AnMon/AnMon_5.75.exe')
+    return engine.get_best_move(fen)
