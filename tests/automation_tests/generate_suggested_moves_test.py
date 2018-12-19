99b90627d8419e11ce327a0fe9bbeb6cd5e3e727
CochiorHeghiLucian <lucian.cochiorheghi@gmail.com>
2018-12-20 00:34:33 +0200

    refactor get suggested moves for automation test

--- tests/automation_tests/generate_suggested_moves_test.py
+++ tests/automation_tests/generate_suggested_moves_test.py
@@ -9,40 +9,73 @@ driver.set_page_load_timeout("30")
 
 generate_moves_btn = driver.find_element_by_id('js-get-moves')
 generate_moves_btn.click()
-time.sleep(10)
+time.sleep(15)
 
-ids_field = driver.find_element_by_id('js-move-ids')
 alphabeta_prunning = driver.find_element_by_id('js-move-alphabeta_prunning')
-negamax = driver.find_element_by_id('js-move-negamax')
-stockfish = driver.find_element_by_id('js-move-stockfish')
+hermann = driver.find_element_by_id('js-move-hermann')
+ids_field = driver.find_element_by_id('js-move-ids')
 minmax = driver.find_element_by_id('js-move-minmax')
+negamax = driver.find_element_by_id('js-move-negamax')
+#random
+ruffian = driver.find_element_by_id('js-move-ruffian')
+rybka = driver.find_element_by_id('js-move-rybka')
+sos = driver.find_element_by_id('js-move-sos')
+spike = driver.find_element_by_id('js-move-spike')
+stockfish = driver.find_element_by_id('js-move-stockfish')
+
 
 print(colored('generate_suggested_moves_button automation tests start...', 'green'))
 
+
+try:
+    assert alphabeta_prunning.text == 'e2e4'
+    print(colored('Alphabeta_prunning field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Alphabeta_prunning field was wrongly generated', 'red'))
+try:
+    assert hermann.text == 'e2e4'
+    print(colored('Herman field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Herman field was wrongly generated', 'red'))
 try:
     assert ids_field.text == 'd2d4'
     print(colored('Ids field has been generated correctly', 'green'))
 except AssertionError:
     print(colored('Ids field was wrongly generated', 'red'))
 try:
-    assert alphabeta_prunning.text == 'd7d5'
-    print(colored('Alphabeta_prunning field has been generated correctly', 'green'))
-except AssertionError:
-    print(colored('Alphabeta_prunning field was wrongly generated', 'red'))
-try:
-    assert negamax.text == 'g7g5'
-    print(colored('Negamax field has been generated correctly', 'green'))
-except AssertionError:
-    print(colored('Negamax field was wrongly generated', 'red'))
-try:
-    assert stockfish.text == 'f8e7'
-    print(colored('Stockfish field has been generated correctly', 'green'))
-except AssertionError:
-    print(colored('Stockfish field was wrongly generated', 'red'))
-try:
     assert minmax.text == 'e2e4'
     print(colored('Minmax field has been generated correctly', 'green'))
 except AssertionError:
     print(colored('Minmax field was wrongly generated', 'red'))
+try:
+    assert negamax.text == 'e2e4'
+    print(colored('Negamax field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Negamax field was wrongly generated', 'red'))
+try:
+    assert ruffian.text == 'e2e4'
+    print(colored('Ruffian field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Ruffian field was wrongly generated', 'red'))
+try:
+    assert rybka.text == 'b1c3'
+    print(colored('Rybka field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Rybka field was wrongly generated', 'red'))
+try:
+    assert sos.text == 'e2e4'
+    print(colored('SOS field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('SOS field was wrongly generated', 'red'))
+try:
+    assert spike.text == 'e2e4'
+    print(colored('Spike field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Spike field was wrongly generated', 'red'))
+try:
+    assert stockfish.text == 'e2e4'
+    print(colored('Stockfish field has been generated correctly', 'green'))
+except AssertionError:
+    print(colored('Stockfish field was wrongly generated', 'red'))
 finally:
     driver.quit()
