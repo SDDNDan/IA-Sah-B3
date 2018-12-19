0ac2bb3efc8c5d84e5b93b4800a6d63390b6d062
gabrielxzc <codrincojocaru@yahoo.com>
2018-12-19 17:44:28 +0200

    reworked list_package_modules function, deleted some useless files

--- application/chess_engines/hermann.py
+++ application/chess_engines/hermann.py
@@ -6,6 +6,6 @@ def get_strategy_name():
 
 
 def get_strategy_move(fen):
-    engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_32.exe')
-    #engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_64.exe')
+    # engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_32.exe')
+    engine = Engine(engine_path='../chess_engines_cpp/Hermann/Hermann28_64.exe')
     return engine.get_best_move(fen)
