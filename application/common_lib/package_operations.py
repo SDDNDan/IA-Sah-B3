c55658cb8a80aea79d6a091c2ffbe0e10e5dca4a
gabrielxzc <codrincojocaru@yahoo.com>
2018-12-19 17:53:49 +0200

    fixed strategies controller with new list package modules function

--- application/common_lib/package_operations.py
+++ application/common_lib/package_operations.py
@@ -8,4 +8,4 @@ def list_package_modules(package_name):
     package = importlib.import_module(package_name)
     prefix = package.__name__ + "."
 
-    return [modname.split for importer, modname, ispkg in pkgutil.iter_modules(package.__path__, prefix) if not ispkg]
+    return [modname for importer, modname, ispkg in pkgutil.iter_modules(package.__path__, prefix) if not ispkg]
--- application/controllers/strategies.py
+++ application/controllers/strategies.py
@@ -5,7 +5,7 @@ CHESS_ENGINES_PACKAGE_NAME = 'application.chess_engines'
 
 
 def get_strategies_controller(request):
-    answer = [{"strategy": strategy.split(".")[0]}
+    answer = [{"strategy": strategy.split(".")[-1]}
               for strategy in list_package_modules(CHESS_ENGINES_PACKAGE_NAME)]
 
     return json.dumps(answer)
