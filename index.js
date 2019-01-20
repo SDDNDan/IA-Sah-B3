c2441098f69d5bbb918a8f15903d6da8017bea20
Alexandru Bodnar <alexandrubodnar10@gmail.com>
2019-01-20 14:07:17 +0200

    old design refined

---
+++ index.js
@@ -0,0 +1,20 @@
+// This function will run a throttled script every 300 ms
+var checkHeader = _.throttle(() => {
+    console.log('checkHeader');
+
+    // Detect scroll position
+    let scrollPosition = Math.round(window.scrollY);
+
+    // If we've scrolled 100px, add "sticky" class to the header
+    if (scrollPosition > 100) {
+        document.querySelector('header').classList.add('sticky');
+    }
+
+    else // If not, remove "sticky" class from header
+    {
+        document.querySelector('header').classList.remove('sticky');
+    }
+}, 10);
+
+// Run the checkHeader function every time you scroll
+window.addEventListener('scroll', checkHeader);
